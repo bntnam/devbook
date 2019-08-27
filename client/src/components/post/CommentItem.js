60af0e1798732cb0ddbf0a3096aef68c2d5955ff
bntnam <bntnam.app@gmail.com>
2019-08-28 00:59:58 +0200

    Add NotFound component

--- client/src/components/post/CommentItem.js
+++ client/src/components/post/CommentItem.js
@@ -1,4 +1,4 @@
-import React, { Fragmanet } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
